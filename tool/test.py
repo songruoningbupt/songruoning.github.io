a14d102a2c76a56f2b784e788f876aa86dcb0283
songruoningbupt <335848046@qq.com>
2018-09-12 22:28:46 +0800

    提交修改

---
+++ tool/test.py
@@ -0,0 +1,8 @@
+import numpy as np
+A = np.random.uniform(0,1,10)
+B = np.random.uniform(0,1,10)
+
+np.einsum('i->', A)       # np.sum(A)
+np.einsum('i,i->i', A, B) # A * B
+np.einsum('i,i', A, B)    # np.inner(A, B)
+np.einsum('i,j->ij', A, B)    # np.outer(A, B)
